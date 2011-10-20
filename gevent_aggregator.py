f5393e09b049f8399d1eba02fa95c5353fe675d4
potekhin <potekhin@in.sports.ru>
2011-10-20 16:00:59 +0400

    Deleted some Trash

--- gevent_aggregator.py
+++ gevent_aggregator.py
@@ -8,32 +8,11 @@ monkey.patch_all()
 import re
 import ujson
 
-def get_content(urlname,url):
-    try:
-        before = time.time()
-        htt = httplib2.Http(timeout=3000)
-        response,content = htt.request(url)
-        print "Request ",urlname," is ",time.time() - before
-        return (urlname,content)
-    except Exception,ex:
-        return (urlname,('error',ex.message))
-
 def to_json(result):
     if type(result[1]) is tuple:
         return """ {"%s":{"error":"%s"}} """%(result[0],result[1][1])
     else:
         return """ {"%s":{"data":%s}} """%(result[0],result[1])
-
-def sync_collect(urls):
-    jobs = [get_content(urlname,url) for urlname,url in urls.items()]
-    result ='{'+','.join([to_json(job) for job in jobs])+'}'
-    return result
-
-def collect(urls):
-    jobs = [gevent.spawn(get_content,urlname,url) for urlname,url in urls.items()]
-    gevent.joinall(jobs,timeout=3000)
-    result ='{'+','.join([to_json(job.value) for job in jobs])+'}'
-    return result
 
 class DependencyAggregator(object):
     regex = re.compile("\{\{\s*(.+?)\s*\}\}")
