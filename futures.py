8db8d96f4319badff62700885d4aaf355e9fb078
potehinre <romanpotekhin@gmail.com>
2012-05-18 17:03:04 +0400

    Error handling in dependencies

--- futures.py
+++ futures.py
@@ -21,24 +21,31 @@ class Future(object):
         timeout.start()
         try:
             htt = httplib2.Http(timeout=self.timeout)
+            dep_greenlets = [param.greenlet for param in self.params if isinstance(param,Future)]
+            gevent.joinall(dep_greenlets)
+            if any('error' in parameter for parameter in self.params.values() if isinstance(parameter,dict)):
+                return {"error":"Dependency Failed"}
             url = self.url.format(**self.params)
             response,content = htt.request(url)
             return {"result":content}
         except Exception as e:
             return {"error":e.message}
         except gevent.Timeout,t:
-            result = (self.url, ('error', 'timeout'))
+            return {'error':'timeout'}
         finally:
             timeout.cancel()
     
     def __getattr__(self,attrname):
         gevent.joinall([self.greenlet])
-        if attrname == 'result' and ('result' in self.greenlet.value):
-            return self.greenlet.value['result']
+        if attrname == 'result':
+            return self.greenlet.value
     
     def __getitem__(self,itemname):
         gevent.joinall([self.greenlet])
-        return ujson.decode(self.greenlet.value['result'])[itemname]
+        if 'result' in self.greenlet.value:
+            return ujson.decode(self.greenlet.value['result'])[itemname]
+        else:
+            return {"error":self.greenlet.value['error']}
     
     def kill(self):
         self.greenlet.kill()
@@ -75,10 +82,10 @@ def joinall(f):
 
 @joinall
 def blog_post_page():
-    haha = CriticalFuture("http://www.dsjajdf.ru?id={id}",timeout=10,id=234)
+    haha = Future("http://www.dsjajdf.ru?id={id}",timeout=10,id=234)
     post = Future("http://www.sports.ru/stat/export/wapsports/blog_post.json?id={id}",timeout=10,id=326627)
     category_posts = Future("http://www.sports.ru/stat/export/wapsports/category_blog_popular_posts.json?category_id={post_category_id}&count=10",
-                            timeout=10,post_category_id=post['blog_id'])
+                            timeout=10,post_category_id=haha['blog_id'])
     return locals()
         
     
