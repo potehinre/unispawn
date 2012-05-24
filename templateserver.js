f7cb69cbcd476f80f42e4cf6242f3312c43523f1
potehinre <romanpotekhin@gmail.com>
2012-05-24 19:03:28 +0400

    Clustering

--- templateserver.js
+++ templateserver.js
@@ -1,7 +1,10 @@
 var http = require('http'),
     url = require('url'),
 	jade = require('jade'),
-	fs = require('fs');
+	fs = require('fs'),
+    cluster = require('cluster');
+
+var numCPUs = require('os').cpus().length;
 
 
 var TemplateCache = function()
@@ -33,57 +36,71 @@ TemplateCache.prototype.getOrCreate = function(path)
 	return this.add(path);
 }
 
-var aggregator = {adress:"127.0.0.1",port:"5000"};
-var templCache = new TemplateCache();
-http.createServer(function(req,resp) {
-	var options = {
-		host:aggregator.adress,
-		port:aggregator.port,
-		path:req.url,
-		method:req.method
-	  };
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+    console.log('worker started');
+  }
+
+  cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+}
+else
+{
+    var aggregator = {adress:"127.0.0.1",port:"5000"};
+    var templCache = new TemplateCache();
+    http.createServer(function(req,resp) {
+	    var options = {
+		    host:aggregator.adress,
+		    port:aggregator.port,
+		    path:req.url,
+		    method:req.method
+	      };
 	
-	//Проксируем запрос к питоновскому бекенду
-	var req = http.request(options,function(res)
-	{
-		var data = '';
-		res.setEncoding('utf8');
-		res.on('data',function(chunk) {
-			data+=chunk;
-		});
-		res.on('end',function()
-		{
-			var fn;
-			if (res.statusCode == 200)
-			{
-                try
-                {
-				    response = JSON.parse(data);
-				    var path = response.template;
-				    var context = response.context;
-				    fn = templCache.getOrCreate(path);
-                }
-                catch(e)
-                {
-                    console.log(e);
-                }
-            
-			}
-			resp.writeHead(res.statusCode,{'Content-Type':'text/html;charset=utf-8'});
-			if (fn != undefined)   
-			{ 
-			 resp.end(fn(context));
-			}
-			else    
-			{ 				   
-				resp.end();
-			}
-		});
-	});
-	req.on('error',function(error)
-	{
-		console.log(error + ' Problems, officer?');
-	});
-	req.end();
-}).listen(1337,'127.0.0.1');
+	    //Проксируем запрос к питоновскому бекенду
+	    var req = http.request(options,function(res)
+	    {
+		    var data = '';
+		    res.setEncoding('utf8');
+		    res.on('data',function(chunk) {
+			    data+=chunk;
+		    });
+		    res.on('end',function()
+		    {
+			    var fn;
+			    if (res.statusCode == 200)
+			    {
+                    try
+                    {
+				        response = JSON.parse(data);
+				        var path = response.template;
+				        var context = response.context;
+				        fn = templCache.getOrCreate(path);
+                    }
+                    catch(e)
+                    {
+                        console.log(e);
+                    }
+                
+			    }
+			    resp.writeHead(res.statusCode,{'Content-Type':'text/html;charset=utf-8'});
+			    if (fn != undefined)   
+			    { 
+			     resp.end(fn(context));
+			    }
+			    else    
+			    { 				   
+				    resp.end();
+			    }
+		    });
+	    });
+	    req.on('error',function(error)
+	    {
+		    console.log(error + ' Problems, officer?');
+	    });
+	    req.end();
+    }).listen(1337,'127.0.0.1');
+}
 console.log('Server running at http://127.0.0.1:1337/');
